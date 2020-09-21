3b31d2d7714c90c7d4b07f3cc5a4278d656174b3
Ben Manning <pookymane@Bens-MacBook-Pro.local>
2020-09-21 19:23:37 -0400

    add solutions for the lab

---
+++ 8_hw_solutions/w01d01/lab/just_the_beginning_SOLUTIONS.js
@@ -0,0 +1,66 @@
+// Correct code
+// console.log("Making your way in the world today takes everything you've got.");
+// console.log("Taking a break from all your worries, sure would help a lot.");
+// console.log("Wouldn't you like to get away?");
+// console.log("Sometimes you want to go");
+// console.log("Where everybody knows your name,");
+// console.log("and they're always glad you came.");
+// console.log("You wanna be where you can see,");
+// console.log("our troubles are all the same");
+// console.log("You wanna be where everybody knows");
+// console.log("Your name.");
+
+// Boolean Expressions
+// 1. false
+// 2. true
+// 3. false
+// 4. false
+// 5. false
+// 6. true
+// 7. true
+// 8. false
+
+// Assignment Operator vs Equality Operator
+// The assignment operator assigns a value to a variable whereas the equality operator check the equality of the values on 
+// both sides of the operator.
+
+// While Loops: Reps and Reps
+
+// 1.
+// let counter = 0;
+// while (counter <= 1000) {
+//   console.log('Ginger chocolate honey patties');
+//   counter++
+// }
+
+// 2. 
+// let counter = 0;
+// while (counter <= 1000) {
+//   console.log(counter)
+//   counter++
+// }
+
+// 3.
+// let counter = 0;
+// while (counter <= 1000) {
+//   console.log("Current loop number is: " + counter)
+//   counter++;
+// }
+
+// For Loops
+// 1.
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i)
+// }
+
+// 2.
+// for (let i = 7; i <= 635; i++) {
+//   console.log(i)
+// }
+
+// 3.
+// let start = 0;
+// const limit = 100;
+// for (start; start<= limit; start++) {
+//   console.log(start);
+// }
