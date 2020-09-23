f517f0550709a113b38110774fa4ffd8f9ac3fc7
Ben Manning <pookymane@Bens-MacBook-Pro.local>
2020-09-22 22:10:47 -0400

    add the solutions for the lab

---
+++ 8_hw_solutions/w01d02/lab/loops_and_booleans_SOLUTIONS.js
@@ -0,0 +1,40 @@
+// Loop it!
+// 1. 
+// for (i = 0; i < 15; i++) {
+//   console.log('hi!')
+// }
+
+// 2.
+// for (let i = 0; i < 30; i++) {
+//   console.log('YEP!')
+// }
+
+// Booleans & Loops Pt. 1
+// for (let i = 0; i < 10; i++) {
+//   let some_num = Math.floor(Math.random() * 40);
+//   console.log(some_num)
+//   if (some_num < 10) {
+//     console.log("that's a small number")
+//   } else if (some_num > 20) {
+//     console.log("that's a big number")
+//   } else {
+//     console.log("that's a medium number")
+//   }
+// }
+
+// Booleans & Loops Pt. 2
+// let fave_day = 'Saturday'
+// let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+// if (fave_day == 'Saturday' || fave_day == 'Sunday') {
+//   console.log('I like the weekend')
+// } else if (weekdays.includes(fave_day)) {
+//   console.log('Give me a good \'ol weekday')
+// } else {
+//   console.log(fave_day + ' is not a day.')
+// }
+
+
+
+
+
